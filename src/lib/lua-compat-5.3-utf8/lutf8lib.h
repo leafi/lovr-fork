64d05073a8ad911deb59f83063d94062e5790f41
leaf <leaf@leaf.sx>
2020-08-06 04:33:33 +0100

    Mod: Include 'utf8' from lua-compat-5.3 backport

---
+++ src/lib/lua-compat-5.3-utf8/lutf8lib.h
@@ -0,0 +1,9 @@
+/* Here's a header for lutf8lib.c... */
+
+#include "api/api.h"
+
+#pragma once
+
+/* luaopen_utf8 gets #defined to luaopen_compat53_utf8 in lprefix.h */
+
+int luaopen_compat53_utf8(lua_State* L);
